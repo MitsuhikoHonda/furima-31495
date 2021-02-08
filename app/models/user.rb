c072122e62cb643bcacf8a763db8a4ea4ff72599
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-08 15:51:44 +0900

    Userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,19 @@ class User < ApplicationRecord
          validates :last_name, presence: true
          validates :first_name, presence: true
          validates :kana_last_name, presence: true
-         validates :kana_last_name, presence: true
+         validates :kana_first_name, presence: true
          validates :birth_date, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
          
-end
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+
+         with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'には全角(カタカナ)を使用してください' } do
+          validates :kana_first_name
+          validates :kana_last_name
+        end
+
+      end
