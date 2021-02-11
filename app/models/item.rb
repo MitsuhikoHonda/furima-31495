3d32b62abdb48e9262db8f554299e1567bdebdd2
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-11 10:20:35 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,20 @@ class Item < ApplicationRecord
   # has_one :purchase_log
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :ship_from
+  belongs_to :day_to_ship
+
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :ship_from_id, presence: true
-  validates :day_to_ship_id, presence: true
+  validates :category_id, numericality: { other_than: 1}
+  validates :condition_id, numericality: { other_than: 1}
+  validates :delivery_fee_id, numericality: { other_than: 1}
+  validates :ship_from_id, numericality: { other_than: 1}
+  validates :day_to_ship_id, numericality: { other_than: 1}
   validates :selling_price, presence: true
 
 end
