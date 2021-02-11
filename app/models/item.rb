3c3d05ebebda5b9fca31654d5b27fa86da81eb60
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-11 15:11:05 +0900

    JSの記述(手数料など数式)

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,13 @@ class Item < ApplicationRecord
   validates :delivery_fee_id, numericality: { other_than: 1}
   validates :ship_from_id, numericality: { other_than: 1}
   validates :day_to_ship_id, numericality: { other_than: 1}
-  validates :selling_price, presence: true
+  
+  with_options presence: true, format: { with: /[3-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-9][0-9][0-9][0-9][0-9]||[1-9][0-9][0-9][0-9][0-9][0-9][1-9][0-9][0-9][0-9][0-9][0-9][0-9]/, message: 'には300円〜9999999円の範囲で半角数字を使用してください' } do
+    validates :selling_price
+  # 上述は綺麗ではないのでメンターさんに模範回答を聞く。
+  # 一度バリデーションがかかると手数料、販売利益の数値が消える。JSの記述方法に問題あり？確認する。
+  end
+
+
 
 end
