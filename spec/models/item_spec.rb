fbc634a2e9e0d39e06d348329dad3e298c2884cf
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-12 13:07:33 +0900

    Itemモデル単体テストの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'categoryを選択せずに投稿できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category を選択してください")
       end
       it 'condition選択せずに投稿できない' do
-        @item.condition_id = '1'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition を選択してください")
       end
       it 'delivery_fee選択せずに投稿できない' do
-        @item.delivery_fee_id = '1'
+        @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee を選択してください")
       end
       it 'ship_from選択せずに投稿できない' do
-        @item.ship_from_id = '1'
+        @item.ship_from_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship from を選択してください")
       end
       it 'day_to_ship選択せずに投稿できない' do
-        @item.day_to_ship_id = '1'
+        @item.day_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Day to ship を選択してください")
       end
@@ -58,17 +58,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Selling price can't be blank")
       end
       it 'selling_priceが299以下では登録できない' do
-        @item.selling_price = '10'
+        @item.selling_price = 10
         @item.valid?
         expect(@item.errors.full_messages).to include("Selling price には300円〜9999999円の範囲で半角数字を使用してください")
       end
       it 'selling_priceが9999999以上では登録できない' do
-        @item.selling_price = '10000000'
+        @item.selling_price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Selling price には300円〜9999999円の範囲で半角数字を使用してください")
       end
       it 'selling_priceが全角では登録できない' do
         @item.selling_price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price には300円〜9999999円の範囲で半角数字を使用してください")
+      end
+      it 'selling_priceが半角英数混合では登録できない' do
+        @item.selling_price = 'a200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price には300円〜9999999円の範囲で半角数字を使用してください")
+      end
+      it 'selling_priceが半角英語だけでは登録できない' do
+        @item.selling_price = 'hundred'
         @item.valid?
         expect(@item.errors.full_messages).to include("Selling price には300円〜9999999円の範囲で半角数字を使用してください")
       end
