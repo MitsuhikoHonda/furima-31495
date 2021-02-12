bc4b2fb5e219d4a961be4220338f512119fe543c
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-12 12:58:15 +0900

    バリデーション記述方法の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,16 +10,20 @@ class Item < ApplicationRecord
   belongs_to :ship_from
   belongs_to :day_to_ship
 
-  validates :image, presence: true
-  validates :item_name, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1, message: 'を選択してください'}
-  validates :condition_id, numericality: { other_than: 1, message: 'を選択してください'}
-  validates :delivery_fee_id, numericality: { other_than: 1, message: 'を選択してください'}
-  validates :ship_from_id, numericality: { other_than: 1, message: 'を選択してください'}
-  validates :day_to_ship_id, numericality: { other_than: 1, message: 'を選択してください'}
-  validates :selling_price, presence: true
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description
+    validates :selling_price
+  end
+  
   validates_inclusion_of :selling_price, in: 300..9999999, message: 'には300円〜9999999円の範囲で半角数字を使用してください'
-
-
+  
+  with_options numericality: { other_than: 1, message: 'を選択してください'} do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :ship_from_id
+    validates :day_to_ship_id
+  end
 end
