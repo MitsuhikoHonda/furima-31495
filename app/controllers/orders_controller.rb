7b1c4fd8cb28eb3387133baa1638032ffa3e4ab0
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-23 22:37:23 +0900

    修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,9 @@ class OrdersController < ApplicationController
   def index
     @order = Order.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
