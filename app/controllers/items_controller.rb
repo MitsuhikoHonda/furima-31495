6eeb48316df547502a119f6a052f433f44c57226
MitsuhikoHonda <mitsuhikohonda0130@gmail.com>
2021-02-27 07:24:25 +0900

    削除機能のユーザーチェック

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :check_user, only: [:edit, :update]
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
